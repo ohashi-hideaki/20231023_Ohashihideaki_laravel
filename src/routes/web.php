4aa795335881d026d5f67275caa258c0a21e2ad9
root <root@DESKTOP-6E96G69>
2023-10-26 18:27:02 +0900

    基本的なアプリのLaravelコード

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
+use app\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +15,9 @@ use App\Http\Controllers\AuthorController;
 */
 
 Route::get('/', [AuthorController::class, 'index']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
+Route::get('/edit', [AuthorController::class, 'edit']);
+Route::post('/edit', [AuthorController::class, 'update']);
+Route::get('/delete', [AuthorController::class, 'delete']);
+Route::post('/delete', [AuthorController::class, 'remove']);
